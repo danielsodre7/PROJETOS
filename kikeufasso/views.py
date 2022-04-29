c854a90b03afd9522f9d7dd91ffaf2eb847623b5
Daniel Pinho Sodre <danielsodre7@gmail.com>
2022-04-28 21:24:27 -0300

    atual

--- kikeufasso/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-
-def home(request):
-    return HttpResponse('HOME')
-# Create your views here.
--- recipes/apps.py
+++ recipes/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class KikeufassoConfig(AppConfig):
+class recipesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kikeufasso'
+    name = 'recipes'
